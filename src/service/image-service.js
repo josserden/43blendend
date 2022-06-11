765950ef1cb14b4a1c4c3d3da96dee49ac6dd30c
MishynMykhailo <misham18092001@gmail.com>
2022-06-11 11:34:26 +0300

    11.06.2022

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -5,11 +5,10 @@ axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
   orientation: 'landscape',
+  per_page: 15,
 };
 
-export const getImages = async query => {
-  const {
-    data: { photos },
-  } = await axios.get(`search?query=${query}`);
-  return photos;
+export const getImages = async (query, page) => {
+  const { data } = await axios.get(`search?query=${query}&page=${page}`);
+  return data;
 };
